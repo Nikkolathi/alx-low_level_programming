89dddab9c59d7d24c8e4938a24d51f915002f9e1
Nicholus Maingi <mainginicholus@gmail.com>
2022-09-08 15:57:23 -0700

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
-* main - Entry point  
+*main - Entry point  
 *   
-* Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
-
-int main(void)
-{
+int main(void){
 int a;
 long int b;
 long long int c;
