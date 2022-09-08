f6aeea42d434a1c42a8d92b4a10606621f20caa2
Nicholus Maingi <mainginicholus@gmail.com>
2022-09-08 15:21:30 -0700

    Prinf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,7 @@
 
 
 /**
- *
- *  * main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  
@@ -13,9 +12,7 @@
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 
 }
