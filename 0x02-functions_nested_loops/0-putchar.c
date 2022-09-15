ace67741d487c39657fc86d3811a297f66f7913a
Nicholus Maingi <mainginicholus@gmail.com>
2022-09-15 10:55:21 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,24 @@
-/**
- *File: 0-putchar.c
- */
-
 #include "main.h"
 
 /**
- * Main  - Prints "putchar" followed by a new line.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
-
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+
+	char c[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 9; i++)
+		
+	{
+	_putchar(c[i]);
+
+	}
+
 	_putchar('\n');
+
 	return (0);
 }
