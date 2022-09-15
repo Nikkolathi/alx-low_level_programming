70daebf88312e0fb6c34e91daa0355d5472943cc
Nicholus Maingi <mainginicholus@gmail.com>
2022-09-15 13:06:56 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,7 +13,7 @@ int main(void)
 {
 int i;
 
- printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 /*
@@ -22,7 +22,7 @@ i = 0;
 * putchar(i);
 *}
 */
- printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
